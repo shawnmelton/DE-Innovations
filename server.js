1d33430f1da3ff496281451b32a3d11012058579
Daniel Burke <daniel.burke@DFRFRM030566.local>
2015-01-30 10:48:45 -0500

    editing copy, updating styles

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+ app.get("/", function(req, res) {
+    res.sendfile(__dirname + '/dist/index.html')
+ });
+ 
+
 app.listen(5000);
